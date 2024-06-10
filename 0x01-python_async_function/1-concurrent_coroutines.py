d5c7df9424d663fd92d830b8cdf34c97c0c6e059
Adejare <adejare.arasheed@gmail.com>
2024-06-10 15:48:58 +0100

    Async I/O

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -2,11 +2,12 @@
 """Execute multiple coroutines at the same time with async"""
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """spawn wait_random n times with the specified max_delay"""
-    all_delays: list = [wait_random(max_delay) for _ in range(n)]
-    result: list = await asyncio.gather(*all_delays)
+    all_delays: List[float] = [wait_random(max_delay) for _ in range(n)]
+    result: List[float] = await asyncio.gather(*all_delays)
     return result
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,11 +2,12 @@
 """Tasks"""
 
 import asyncio
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> list:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """spawn wait_random n times with the specified max_delay"""
-    all_delays: list = [task_wait_random(max_delay) for _ in range(n)]
+    all_delays = [task_wait_random(max_delay) for _ in range(n)]
     result = await asyncio.gather(*all_delays)
     return result
