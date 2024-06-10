1a6ba52089a7b3f1722661598cb507e2379e753a
Adejare <adejare.arasheed@gmail.com>
2024-06-10 17:30:40 +0100

    Asyncio I/O

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -10,4 +10,4 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """spawn wait_random n times with the specified max_delay"""
     all_delays = [task_wait_random(max_delay) for _ in range(n)]
     result = await asyncio.gather(*all_delays)
-    return result.sort()
+    return sorted(result)
