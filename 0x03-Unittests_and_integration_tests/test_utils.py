4cb3953656d3a8d45dc3631700cffc8ea81f2fd4
Adejare <adejare.arasheed@gmail.com>
2024-06-30 17:43:43 +0100

    Unittests and Intgration Tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest.mock
 from parameterized import parameterized
 import unittest
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import requests
 
 
@@ -45,3 +45,35 @@ class TestGetJson(unittest.TestCase):
 
         mock.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ unittest for memoize decorator """
+    def test_memoize(self):
+        """ memoize decorator testing """
+        class TestClass:
+            """ testing """
+            def a_method(self):
+                """ testing a_method for memoize """
+                return 42
+
+            @memoize  # makes a_property  behaves like attribute
+            def a_property(self):
+                """create a property"""
+                return self.a_method()
+
+        with unittest.mock.patch.object(
+            TestClass, 'a_method', return_value=42) as mock_method:
+
+            obj_inst = TestClass()
+
+            # Call a_property twice
+            result1 = obj_inst.a_property
+            result2 = obj_inst.a_property
+
+            # Assert a_method was called only once
+            mock_method.assert_called_once()
+
+            # Assert correct results
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
